0c942917451fe7307a58fc4fb6c836b69965476d
SenkoVlad <vlad.senko2011@yandex.ru>
2021-06-09 14:10:08 +0300

    Add project files.

---
+++ XUnit.Tests/DemoClassesTests.cs
@@ -0,0 +1,33 @@
+﻿using Calabonga.DemoClasses;
+using System;
+using Xunit;
+
+namespace XUnit.Tests
+{
+    public class DemoClassesTests
+    {
+        [Fact]
+        public void ItShould_under_testing()
+        {
+            //Arrange
+            Person person = new Person();
+
+            //Act
+
+            //Assert
+            Assert.NotNull(person);
+        }
+
+        [Fact]
+        public void ItShould_throw_exception_when_person_is_null()
+        {
+            //Arrange
+            Person person = null;
+
+            //Act
+
+            //Assertt
+            Assert.Throws<NullReferenceException>(() => person.Name);
+        }
+    }
+}
